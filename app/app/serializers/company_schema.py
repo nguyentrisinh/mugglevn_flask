6a6be10adede297bd964b605851c7a60917b2292
Unknown <nguyentrisinh0810@gmail.com>
2017-08-01 17:07:00 +0700

    fix ta lua

--- app/app/serializers/company_schema.py
+++ app/app/serializers/company_schema.py
@@ -1,8 +1,8 @@
 from . import ma
+from ..models import Company
 
 
 class CompanySchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'slug', 'website', 'size', 'rating', 'bio', 'overview', 'email', 'avatar',
                   'address', 'google_map', 'job_count', 'review_count', 'created_at', 'updated_at', 'type_id')
-
